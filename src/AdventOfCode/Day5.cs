bfd3c548fa83cdf0d6af099f87ef9f4a38e71009
BrownKnight <amanrk@hotmail.co.uk>
2021-12-23 15:22:03 +0000

    fix day 5 part 1, add day 5 part 2

--- src/AdventOfCode/Day5.cs
+++ src/AdventOfCode/Day5.cs
@@ -19,31 +19,90 @@ public class Day5
             var point2 = pointMapSplit[1].Split(',').Select(int.Parse).ToArray();
             var (x1, y1, x2, y2) = (point1[0], point1[1], point2[0], point2[1]);
 
-            if (x1 == x2 && y1 == y2) continue;
-
             // only operate on horizontal or vertical lines and increment value when adding a line
             if (x1 == x2)
             {
                 var lowerBound = Math.Min(y1, y2);
-                var upperBound = Math.Max(y1, y2);
+                var upperBound = Math.Max(y1, y2) + 1;
 
-                var valuesToChange = board[x1][lowerBound..upperBound];
-                for (int i = 0; i < valuesToChange.Length; i++)
+                for (int i = lowerBound; i < upperBound; i++)
                 {
-                    valuesToChange[i]++;
+                    board[x1][i]++;
                 }
             }
 
             if (y1 == y2)
             {
                 var lowerBound = Math.Min(x1, x2);
-                var upperBound = Math.Max(x1, x2);
+                var upperBound = Math.Max(x1, x2) + 1;
                 var valuesToChange = board[lowerBound..upperBound];
                 foreach (var value in valuesToChange)
                 {
                     value[y1]++;
                 }
             }
+        }
+
+        return board.SelectMany(x => x).Count(x => x >= 2);
+    }
+
+    public static int ProcessInputFindOverlappingWithDiagonal(string[] pointMaps)
+    {
+        var board = new int[1000][];
+        for (int i = 0; i < board.Length; i++)
+        {
+            board[i] = new int[1000];
+        }
+
+        // Populate lines across the array one by one
+        foreach (var pointMap in pointMaps)
+        {
+            var pointMapSplit = pointMap.Split(" -> ");
+            var point1 = pointMapSplit[0].Split(',').Select(int.Parse).ToArray();
+            var point2 = pointMapSplit[1].Split(',').Select(int.Parse).ToArray();
+            var (x1, y1, x2, y2) = (point1[0], point1[1], point2[0], point2[1]);
+
+            // O on horizontal or vertical lines and increment value when adding a line
+            if (x1 == x2)
+            {
+                var lowerBound = Math.Min(y1, y2);
+                var upperBound = Math.Max(y1, y2) + 1;
+
+                for (int i = lowerBound; i < upperBound; i++)
+                {
+                    board[x1][i]++;
+                }
+            }
+            else if (y1 == y2)
+            {
+                var lowerBound = Math.Min(x1, x2);
+                var upperBound = Math.Max(x1, x2) + 1;
+                var valuesToChange = board[lowerBound..upperBound];
+                foreach (var value in valuesToChange)
+                {
+                    value[y1]++;
+                }
+            }
+            // If the line is a diagonal, add that line
+            else
+            {
+                var rangeX = Enumerable.Range(Math.Min(x1, x2), Math.Max(x1, x2) + 1 - Math.Min(x1, x2));
+                var rangeY = Enumerable.Range(Math.Min(y1, y2), Math.Max(y1, y2) + 1 - Math.Min(y1, y2));
+                if (x1 > x2)
+                {
+                    rangeX = rangeX.Reverse();
+                }
+                if (y1 > y2)
+                {
+                    rangeY = rangeY.Reverse();
+                }
+                var coords = Enumerable.Zip(rangeX, rangeY);
+                foreach(var (x, y) in coords)
+                {
+                    board[x][y]++;
+                }
+            }
+
         }
 
         return board.SelectMany(x => x).Count(x => x >= 2);
--- test/AdventOfCodeUnitTests/Day5UnitTests.cs
+++ test/AdventOfCodeUnitTests/Day5UnitTests.cs
@@ -16,13 +16,13 @@ public class Day5UnitTests
         var numOverlappingPoints = Day5.ProcessInputFindOverlapping(data);
 
         Console.WriteLine($"Day5.1: {numOverlappingPoints}");
-        //var lastWinningBoardScore = Day4.ProcessInputForFinalWinningBoard(data);
+        var numOverlappingPointsWithDiagonals = Day5.ProcessInputFindOverlappingWithDiagonal(data);
 
-        //Console.WriteLine($"Day4.2: {lastWinningBoardScore}");
+        Console.WriteLine($"Day5.2: {numOverlappingPointsWithDiagonals}");
     }
 
     [Fact]
-    public void Verify()
+    public void VerifyVerticalAndHorizontal()
     {
         var data = new[]
         {
@@ -39,6 +39,27 @@ public class Day5UnitTests
         };
         var numOverlappingPoints = Day5.ProcessInputFindOverlapping(data);
 
-        Assert.Equal(2, numOverlappingPoints);
+        Assert.Equal(5, numOverlappingPoints);
+    }
+
+    [Fact]
+    public void VerifyVerticalAndHorizontalAndDiagonal()
+    {
+        var data = new[]
+        {
+            "0,9 -> 5,9",
+            "8,0 -> 0,8",
+            "9,4 -> 3,4",
+            "2,2 -> 2,1",
+            "7,0 -> 7,4",
+            "6,4 -> 2,0",
+            "0,9 -> 2,9",
+            "3,4 -> 1,4",
+            "0,0 -> 8,8",
+            "5,5 -> 8,2",
+        };
+        var numOverlappingPoints = Day5.ProcessInputFindOverlappingWithDiagonal(data);
+
+        Assert.Equal(12, numOverlappingPoints);
     }
 }
